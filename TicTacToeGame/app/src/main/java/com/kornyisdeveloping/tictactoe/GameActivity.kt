4c77371b84d1708c4c0e7c34ca60ce0604f5788c
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-03-14 14:07:06 +0200

    first button works
    created the first button for the offlne mode

--- TicTacToeGame/app/src/main/java/com/kornyisdeveloping/tictactoe/GameActivity.kt
+++ TicTacToeGame/app/src/main/java/com/kornyisdeveloping/tictactoe/GameActivity.kt
@@ -1,38 +1,20 @@
 package com.kornyisdeveloping.tictactoe
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.kornyisdeveloping.tictactoe.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-
-    lateinit var binding : ActivityMainBinding
+class GameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(binding.root)
-
-        binding.playOfflineBtn.setOnClickListener {
-            createOfflineGame()
-        }
-
+        setContentView(R.layout.activity_game)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-
-    fun createOfflineGame() {
-        startGame()
-    }
-
-    fun startGame() {
-        startActivity(Intent(this, GameActivity::class.java))
     }
 }
