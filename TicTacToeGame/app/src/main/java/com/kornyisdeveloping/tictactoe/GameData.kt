eb6da92fdbb27fea495f6f594ccb0a2ae2fe306e
KornyIsDeveloping <132944959+KornyIsDeveloping@users.noreply.github.com>
2024-03-18 15:48:58 +0200

    online game and firebase
    implementing the online game feature using the firebase for storing the games sessions

--- TicTacToeGame/app/src/main/java/com/kornyisdeveloping/tictactoe/GameData.kt
+++ TicTacToeGame/app/src/main/java/com/kornyisdeveloping/tictactoe/GameData.kt
@@ -2,12 +2,20 @@ package com.kornyisdeveloping.tictactoe
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 object GameData {
     private var _gameModel : MutableLiveData<GameModel> = MutableLiveData()
     var gameModel : LiveData<GameModel> = _gameModel
+    var myId = ""
 
     fun saveGameModel(model : GameModel) {
         _gameModel.postValue(model)
+        if(model.gameId != "-1") {
+            Firebase.firestore.collection("games")
+                .document(model.gameId)
+                .set(model)
+        }
     }
 }
--- TicTacToeGame/app/src/main/java/com/kornyisdeveloping/tictactoe/MainActivity.kt
+++ TicTacToeGame/app/src/main/java/com/kornyisdeveloping/tictactoe/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.kornyisdeveloping.tictactoe.databinding.ActivityMainBinding
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,6 +20,14 @@ class MainActivity : AppCompatActivity() {
 
         binding.playOfflineBtn.setOnClickListener {
             createOfflineGame()
+        }
+
+        binding.createOnlineGameBtn.setOnClickListener {
+            createOnlineGame()
+        }
+
+        binding.joinOnlineGameBtn.setOnClickListener {
+            joinOnlineGame()
         }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -35,6 +44,33 @@ class MainActivity : AppCompatActivity() {
             )
         )
         startGame()
+    }
+
+    fun createOnlineGame() {
+//        GameData.saveGameModel(
+//            GameModel(
+//                gameStatus = GameStatus.CREATED
+//                gameId = Random.nextInt(1000..9999).toString()
+//            )
+//        )
+//        startGame()
+        GameData.myId = "X"
+        val newGameId = Random.nextInt().toString()  // Generate a new game ID as a string
+        GameData.saveGameModel(
+            GameModel(
+                gameId = newGameId,  // Set the new game ID here
+                gameStatus = GameStatus.CREATED,
+                filledPos = mutableListOf("", "", "", "", "", "", "", "", ""),
+                winner = "",
+                currentPlayer = "X"
+                // Other properties can be set here as well
+            )
+        )
+        startGame()
+    }
+
+    fun joinOnlineGame() {
+
     }
 
     fun startGame() {
